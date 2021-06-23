c1f1302070e99a8ca28c6fe08b98aa92f0148613
Stan Switzer <sjswitzer@gmail.com>
2021-06-23 12:37:43 -0700

    another try with sevice workers

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,13 @@
   // I have no particular need for a service worker, but it's needed for a PWA
-   self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-      // caches.match() always resolves
-      // but in case of success response will have value
-      if (response !== undefined) {
+   self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(response => {
+      if (response) {
         return response;
       } else {
-        return fetch(event.request).then(function (response) {
-          // response may be used only once
-          // we need to save clone to put one copy in cache
-          // and serve second one
-          let responseClone = response.clone();
-          
-          caches.open('v1').then(function (cache) {
-            cache.put(event.request, responseClone);
-          });
+        return fetch(event.request).then(response => {
+          let cloned = response.clone();
+          caches.open('v1').then(cache => cache.put(event.request, cloned));
           return response;
-        }).catch(function () {
-          return caches.match('/sw-test/gallery/myLittleVader.jpg');
         });
       }
     }));
