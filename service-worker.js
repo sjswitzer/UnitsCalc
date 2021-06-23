540e51eb96f5a198e038c6574779efc0ca86426d
Stan Switzer <sjswitzer@gmail.com>
2021-06-23 13:36:28 -0700

    try passing version to service worker

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,22 @@
-  // I have no particular need for a service worker, but it's needed for a PWA
-   self.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request).then(response => {
-      if (response) {
-        return response;
-      } else {
-        return fetch(event.request).then(response => {
-          let cloned = response.clone();
-          caches.open('v1').then(cache => cache.put(event.request, cloned));
-          return response;
-        });
-      }
-    }));
-  });
+// I have no particular need for a service worker, but it's needed for a PWA
+let version = "v0.0";
+
+onmessage = e => {
+    console.log("onmessage", e);
+    if (e.data.version)
+      version = e.data.version;
+    // the passed-in data is available via e.data
+};
+
+onfetch = event => {
+  console.log("onfetch", e);
+  event.respondWith(caches.match(event.request).then(response => {
+  if (response)
+    return response;
+  return fetch(event.request).then(response => {
+    let cloned = response.clone();
+    caches.open(version).then(cache => cache.put(event.request, cloned));
+    return response;
+    });
+  }));
+};
