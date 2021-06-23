e209e5cdf169154259ac20be9865e1ce15d837ce
Stan Switzer <sjswitzer@gmail.com>
2021-06-23 13:49:44 -0700

    oops

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,14 @@
 // I have no particular need for a service worker, but it's needed for a PWA
 let version = "v0.0";
 
-onmessage = e => {
+onmessage = event => {
     console.log("onmessage", e);
-    if (e.data.version)
-      version = e.data.version;
-    // the passed-in data is available via e.data
+    if (event.data.version)
+      version = event.data.version;
 };
 
 onfetch = event => {
-  console.log("onfetch", e);
+  console.log("onfetch", event);
   event.respondWith(caches.match(event.request).then(response => {
   if (response)
     return response;
