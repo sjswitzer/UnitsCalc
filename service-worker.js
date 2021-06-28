51279bb7f1e26cd63e5173a8dce185d893571a03
Stan Switzer <sjswitzer@gmail.com>
2021-06-27 23:54:42 -0700

    Update support

--- service-worker.js
+++ service-worker.js
@@ -3,12 +3,16 @@
 onfetch = event => {
   console.log("onfetch", event);
   event.respondWith(caches.match(event.request).then(response => {
+    // Issue a fetch regardless
+    let fetchRequest = fetch(event.request).then(fetchResponse => {
+      let clonedResponse = fetchResponse.clone();
+      caches.open("UnitsCalc-v1").then(cache => cache.put(event.request, clonedResponse));
+      return fetchResponse;
+    });
+    // If the cache had a response, use it immediately
     if (response)
       return response;
-    return fetch(event.request).then(response => {
-      let clonedResponse = response.clone();
-      caches.open("UnitsCalc-v1").then(cache => cache.put(event.request, clonedResponse));
-      return response;
-    });
+    // Otherwise use the result of the fetch
+    return fetchRequest;
   }));
 };
