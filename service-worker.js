c8e31ecdb03da20b0c798b2370905be029b415de
Stan Switzer <sjswitzer@gmail.com>
2021-06-28 11:38:53 -0700

    more logging in srvice worker

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,24 @@
 // I have no particular need for a service worker, but it's necessary for a PWA.
 
 onfetch = event => {
-  console.log("onfetch", event);
+  console.info("onfetch", event);
   event.respondWith(caches.match(event.request).then(response => {
     // Issue a fetch regardless
     let fetchRequest = fetch(event.request).then(fetchResponse => {
       let clonedResponse = fetchResponse.clone();
       caches.open("UnitsCalc-v1").then(cache => {
         cache.put(event.request, clonedResponse);
-        console.log("cached", event, clonedResponse);
+        console.info("cached", event, clonedResponse);
       });
       return fetchResponse;
     });
     // If the cache had a response, use it immediately
-    if (response)
+    if (response) {
+      console.info("cached response", event, response)
       return response;
+    }
     // Otherwise use the result of the fetch
+    console.info("fetched response", event, fetchRequest)
     return fetchRequest;
   }));
 };
