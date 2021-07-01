0c8a13e391564ae61a7e83def724d558729f9aaa
Stan Switzer <sjswitzer@gmail.com>
2021-06-30 17:16:16 -0700

    final touches on ser ice worker

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,47 @@
 // I have no particular need for a service worker, but it's necessary for a PWA.
 
+let logging = false;
 let cacheName = location.pathname;  // Segregate caching by worker location
 
 onfetch = event => {
-  console.info("onfetch", event.request);
+  if (logging) console.info("onfetch", event.request);
   // There should be async blocks:
   //     async { ... }
   // Instead, I'll use
   //     (async () => { ... })()
   event.respondWith((async () => {
     let cache = await caches.open(cacheName);
-    console.info("cache", cache);
+    if (logging) console.info("cache", cache);
     let cacheResponse = await cache.match(event.request);
-    console.info("cacheResponse", cacheResponse);
+    if (logging) console.info("cacheResponse", cacheResponse);
     // Issue a fetch request regardless
     let fetchResult = fetch(event.request).then(fetchResponse => {
       if (fetchResponse.ok) {
-        console.info("successful response", fetchResponse);
+        if (logging) console.info("successful response", fetchResponse);
         let clonedResponse = fetchResponse.clone();
         cache.put(event.request, clonedResponse);
-        console.info("cached", event, clonedResponse);
+        if (logging) console.info("cached", event, clonedResponse);
         return fetchResponse;  // succeed with the response
       }
-      console.info("failed response", fetchResponse);
+      if (logging) console.info("failed response", fetchResponse);
       // failed, return the cached response or the failure if none.
       return cacheResponse ?? fetchResponse;
     });
     if (!cacheResponse) {
-      console.info("uncached", fetchResult);
+      if (logging) console.info("uncached", fetchResult);
       return fetchResult;
     }
     // Wait for a moment and return the cached value
+    //   The hope is that mobile devices will fail requests immediately
+    //   when fully offline, but a half second is not so long to wait.
     let timer = new Promise(resolve => {
       setTimeout(() => {
-        console.info("timeout", cacheResponse);
+        if (logging) console.info("timeout", cacheResponse);
         resolve(cacheResponse);
       }, 500);
     });
     let res = await Promise.any([timer, fetchResult]);
-    console.info("resolved with", res);
+    if (logging) console.info("resolved with", res);
     return res;
   })());
 };
